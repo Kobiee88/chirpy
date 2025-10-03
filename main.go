15ea97fe15f3856e1c224924ad9c3d9093ba534d
Kobiee88 <thomas.mysliwietz@gmail.com>
2025-10-03 23:16:39 +0200

    B: Added content of chapters 2 and 3

--- main.go
+++ main.go
@@ -4,21 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	// Create an instance of apiConfig
+	apiCfg := apiConfig{}
+
 	// Create a new ServeMux
 	mux := http.NewServeMux()
 
 	// Add a simple handler for the health check endpoint
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
+
+	// Add a handler to display the metrics
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", apiCfg.fileserverHits.Load())))
+	})
+
+	// Add a handler to reset the metrics
+	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Store(0)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hits reset to 0"))
 	})
 
 	// Serve the static HTML file
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
 	// Create the server
 	server := &http.Server{
@@ -28,4 +50,14 @@ func main() {
 
 	fmt.Println("Starting server on localhost:8080...")
 	log.Fatal(server.ListenAndServe())
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Increment the fileserverHits counter
+		cfg.fileserverHits.Add(1)
+
+		// Call the next handler in the chain
+		next.ServeHTTP(w, r)
+	})
 }
